3db69febbed315bc92713c15060bf4359589b94d
tartaman <70653340+tartaman@users.noreply.github.com>
2025-02-11 11:01:23 -0600

    Maqueracion
    MAquetado de users

--- DjangoBasics/modelo_app/users/views.py
+++ DjangoBasics/modelo_app/users/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from .models import user, user_address
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the users index.")
+def indexUsers(request):
+    users = user.objects.all()
+    data = {
+        "users": users,
+        "title": "Users List"
+    }
+    return render(request, "users/index.html", data)
