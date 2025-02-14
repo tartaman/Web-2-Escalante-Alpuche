7fc24219d4c734c53cec8db29b3f5280bbab2828
tartaman <70653340+tartaman@users.noreply.github.com>
2025-02-13 21:01:23 -0600

    more tuff

--- DjangoBasics/modelo_app/users/urls.py
+++ DjangoBasics/modelo_app/users/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indexUsers, name="indexUsers")
+    path("", views.indexUsers, name="indexUsers"),
+    path("create", views.createUsersView, name="createUsersView"),
+    path("createUser", views.createUser, name="createUser"),
+    path("details/<int:id>", views.userDetail, name="userDetail")
 ]
--- DjangoBasics/modelo_app/users/views.py
+++ DjangoBasics/modelo_app/users/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from .models import user, user_address
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 
 def indexUsers(request):
     users = user.objects.all()
@@ -10,3 +9,40 @@ def indexUsers(request):
         "title": "Users List"
     }
     return render(request, "users/index.html", data)
+
+def createUsersView(request):
+    return render(request, "users/create.html")
+
+def createUser(request):
+    data = {}
+    try:
+        if request.method == "POST":
+            name = request.POST["name"]
+            email = request.POST["email"]
+            age = request.POST["age"] 
+            rfc = request.POST["rfc"]
+            photo = request.POST.get("photo")
+
+            User = user(
+                name=name,
+                email=email,
+                age=age,
+                rfc=rfc,
+                photo=photo
+            )
+            User.save()
+            data["user"] = User
+            data["message"] = "User created successfully"
+            data["status"] = "success"
+    except Exception as e:
+        data["message"] = str(e)
+        data["status"] = "error"
+
+    return render(request, "users/create.html", data)
+
+def userDetail(request, id):
+    User = get_object_or_404(user,id=id)
+    data = {
+        "user": User
+    }
+    return render(request, "users/detail.html", data)
