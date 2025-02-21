c9f165a0c3379d31ea69b13aa88e837d5d755f80
tartaman <70653340+tartaman@users.noreply.github.com>
2025-02-21 11:50:37 -0600

    listo los css y js y ya muestra dir de users

--- DjangoBasics/modelo_app/users/urls.py
+++ DjangoBasics/modelo_app/users/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indexUsers, name="indexUsers"),
+    path("all", views.indexUsers, name="indexUsers"),
     path("create", views.createUsersView, name="createUsersView"),
     path("createUser", views.createUser, name="createUser"),
     path("details/<int:id>", views.userDetail, name="userDetail"),
-    path("createUser-by-fetch", views.createUserByFetch, name="createUser-by-fetch")
+    path("createUser-by-fetch", views.createUserByFetch, name="createUser-by-fetch"),
+    path("", views.landing, name="landing"),
+    path("update/<int:id>", views.updateUser, name="updateUser")
 ]
--- DjangoBasics/modelo_app/users/views.py
+++ DjangoBasics/modelo_app/users/views.py
@@ -3,7 +3,6 @@ from .models import user, user_address
 from django.shortcuts import render, get_object_or_404
 import json
 from django.http import JsonResponse
-
 def indexUsers(request):
     users = user.objects.all()
     data = {
@@ -11,7 +10,8 @@ def indexUsers(request):
         "title": "Users List"
     }
     return render(request, "users/index.html", data)
-
+def landing(request):
+    return render(request, "users/landing.html")
 def createUsersView(request):
     return render(request, "users/create.html")
 def createUserByFetch(request):
@@ -46,8 +46,38 @@ def createUser(request):
     return render(request, "users/create.html", data)
 
 def userDetail(request, id):
-    User = get_object_or_404(user,id=id)
+    User = get_object_or_404(user, id=id)
+    addresses = User.user_address_set.all()  # Obtiene todas las direcciones relacionadas
+
     data = {
-        "user": User
+        "user": User,
+        "addresses": addresses  # Pasamos las direcciones al template
     }
     return render(request, "users/detail.html", data)
+
+def updateUser(request, id):
+    data = {}
+    try:
+        User = get_object_or_404(user, id=id)
+        name = request.POST["name"]
+        email = request.POST["email"]
+        age = request.POST["age"] 
+        rfc = request.POST["rfc"]
+        photo = request.POST.get("photo")
+
+        
+        User.name=name
+        User.email=email
+        User.age=age
+        User.rfc=rfc
+        User.photo=photo
+        User.save()
+        data["users"] = user.objects.all()
+        data["user"] = User
+        data["message"] = "User created successfully"
+        data["status"] = "success"
+    except Exception as e:
+        data["message"] = str(e)
+        data["status"] = "error"
+
+    return render(request, f"users/index.html", data)
