ee3d5ce8294042d38caea29dbf6034551d4b7786
tartaman <70653340+tartaman@users.noreply.github.com>
2025-03-05 02:27:14 -0600

    ia

--- DjangoBasics/modelo_app/examen/models.py
+++ DjangoBasics/modelo_app/examen/models.py
@@ -17,7 +17,7 @@ class productos(models.Model):
 class eventos(models.Model):
     name = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=500, default="No hay descripción", blank=True, null=True)
-    imagen_url = models.CharField(max_length=500, default="https://i.pinimg.com/170x/56/6a/8b/566a8b03ee78dd47250c559e26906df9.jpg")
+    imagen_url = models.CharField(max_length=500, default="https://img.freepik.com/vector-gratis/carnaval-brasileno-plano-creativo_23-2148404612.jpg?t=st=1741162794~exp=1741166394~hmac=01fa8f924797fe3952a086af7d70843819d9594470aa83abb3f2c20081f18117&w=900")
     fechaInicio = models.DateTimeField()
     fechafin = models.DateTimeField()
     localidad_id = models.ForeignKey(localidad, on_delete=models.CASCADE)
