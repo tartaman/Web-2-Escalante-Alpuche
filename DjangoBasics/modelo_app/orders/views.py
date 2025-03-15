162b77376ee69f059688c175b6d8da62ded08e99
tartaman <70653340+tartaman@users.noreply.github.com>
2025-03-14 19:27:36 -0600

    yam

--- DjangoBasics/modelo_app/orders/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls orders index. ")
--- DjangoBasics/modelo_app/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import user, user_address
-# Register your models here.
-
-admin.site.register(user)
-admin.site.register(user_address)
