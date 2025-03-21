3e4ff8f0d2c4b9ea8ad9d14706a965a6a1f7c189
tartaman <70653340+tartaman@users.noreply.github.com>
2025-03-21 17:11:56 -0600

    yam

--- reactsote/src/page/Login.js
+++ reactsote/src/page/Login.js
@@ -1,16 +1,17 @@
 import "../style/Login.css";
 export default function Login() {
+    document.querySelector("body").style.backgroundColor = "#0D081C";
     return (
         <div className="containerlogin">
             <h3>Login</h3>
             <form className="formLogin">
                 <div>
-                    <label htmlFor="username">correo:</label>
+                    <label htmlFor="username">Correo</label>
                     <input className="login-input" type="text" id="username" name="username" placeholder="Jane Doe"/>
                 </div>
                 <div>
-                    <label htmlFor="password">contraseña:</label>
-                    <input className="login-input" type="password" id="password" name="password" placeholder="password" />
+                    <label htmlFor="password">Pass</label>
+                    <input className="login-input" type="password" id="password" name="password" placeholder="Password" />
                 </div>
                 <div className="login-button-container">
                     <button type="submit" className="login-button">Login</button>
--- reactsote/src/page/Products.js
+++ reactsote/src/page/Products.js
@@ -1,7 +1,12 @@
 import ProductListItem from "../components/ProductListItem"
+import "../style/products.css"
 export default function Products() {
     return (
         <div>
+            <div className="articles">
+                <h1 >We release interesting articles <br/> about technology</h1>
+                <p className="barritas">////////////////////////////////////////////////////</p>
+            </div>
             <ProductListItem />
         </div>
     )
