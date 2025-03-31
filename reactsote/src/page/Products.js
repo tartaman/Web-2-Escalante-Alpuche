b68fc797a7628ece6e36d7b0bd78431df4ed9650
tartaman <70653340+tartaman@users.noreply.github.com>
2025-03-31 05:14:11 -0600

    diseño

--- reactsote/src/page/Products.js
+++ reactsote/src/page/Products.js
@@ -54,7 +54,7 @@ export default function Products() {
                 <p className="barritas">///////////////////////////////////////////////////</p>
             </div>
             <div style={{ display: "flex", justifyContent: "center" }}>
-                <input onChange={(e) => setWord(e.target.value)} placeholder="Buscar productos..." />
+                <input className="search" onChange={(e) => setWord(e.target.value)} placeholder="Buscar productos..." />
             </div>
 
             {loading && <p style={{ textAlign: "center" }}>Cargando productos...</p>}
